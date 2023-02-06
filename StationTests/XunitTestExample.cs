255c3c669ca51e409189ddc4e02d9b75495e102c
Edward Coad <ecoad@lumination.com.au>
2023-02-06 14:21:48 +1030

    Skeleton Test framework project added

---
+++ StationTests/XunitTestExample.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Xunit;
+using System.IO;
+
+namespace StationTests
+{
+    public class XunitTestExample
+    {
+        private const string Expected = "Hello World!";
+
+        [Fact]
+        public void Test1()
+        {
+            var result = "Hello World!";
+            Assert.Equal(Expected, result);
+        }
+    }
+}
