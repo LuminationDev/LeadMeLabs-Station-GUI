791ae6db0cf85c0ce61d76c2eea3f730081e1ade
Edward Michael Coad <ecoad@lumination.com.au>
2023-10-27 10:45:22 +1030

    AMD Fix

--- Station/_qa/checks/SoftwareChecks.cs
+++ Station/_qa/checks/SoftwareChecks.cs
@@ -4,8 +4,6 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Management.Automation;
-using Microsoft.Win32;
-using Namotion.Reflection;
 
 namespace Station._qa.checks;
 
@@ -233,44 +231,31 @@ public class SoftwareChecks
     private QaCheck IsAmdInstalled()
     {
         QaCheck qaCheck = new QaCheck("amd_installed");
-        const string adrenalinSearchKey = @"SOFTWARE\AMD";
-        const string adrenalinValueName = "DisplayName";
-        const string adrenalinValueExpected = "AMD Radeon Software";
 
         try
         {
-            using (RegistryKey key = Registry.CurrentUser.OpenSubKey(adrenalinSearchKey))
+            using PowerShell powerShell = PowerShell.Create();
+            powerShell.AddCommand("Get-ItemProperty")
+                .AddParameter("Path", "HKLM:\\Software\\AMD\\CN");
+            var results = powerShell.Invoke();
+
+            if (results.Count > 0 && results[0] != null)
             {
-                if (key != null)
+                var adrenalin = results[0].Properties["Adrenalin"]?.Value.ToString();
+                if (adrenalin is "True")
                 {
-                    foreach (string subKeyName in key.GetSubKeyNames())
-                    {
-                        using (RegistryKey subKey = key.OpenSubKey(subKeyName))
-                        {
-                            if (subKey != null)
-                            {
-                                object value = subKey.GetValue(adrenalinValueName);
-                                if (value != null && value.ToString() == adrenalinValueExpected)
-                                {
-                                    qaCheck.SetPassed(null);
-                                    return qaCheck;
-                                }
-                            }
-                        }
-                    }
+                    qaCheck.SetPassed(null);
+                    return qaCheck;
                 }
             }
-
-            Console.WriteLine("AMD Adrenalin is not installed.");
             qaCheck.SetFailed("AMD Adrenalin is not installed.");
-            return qaCheck;
         }
         catch (Exception ex)
         {
-            Console.WriteLine($"Error: {ex.Message}");
             qaCheck.SetFailed($"Error: {ex.Message}");
-            return qaCheck;
         }
+
+        return qaCheck;
     }
     
     /// <summary>
