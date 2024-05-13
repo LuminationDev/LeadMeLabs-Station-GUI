8c0cc4afb785deb4786bd026b1e4423d4ce33481
dkearns-lumination <dkearns@lumination.com.au>
2024-05-13 10:37:44 +0930

    Add backup network check

--- Station/Components/_utils/Network.cs
+++ Station/Components/_utils/Network.cs
@@ -15,7 +15,13 @@ public static class Network
             httpClient.Timeout = TimeSpan.FromSeconds(10);
             var response = httpClient.GetAsync("https://leadme-internal.sgp1.vultrobjects.com/Station/version").GetAwaiter().GetResult();
             connected = response.IsSuccessStatusCode;
-            return response.IsSuccessStatusCode;
+            if (!connected)
+            {
+                // backup in case they haven't allowlisted vultr yet
+                var responseBackup = httpClient.GetAsync("http://learninglablauncher.herokuapp.com/program-station-version").GetAwaiter().GetResult();
+                connected = responseBackup.IsSuccessStatusCode;
+            }
+            return connected;
         }
         catch
         {
